729678cae5d6e2d341ebe2147bce59943dca5d39
Erik de Jonge <erik@a8.nl>
2015-03-06 15:13:01 +0100

    -

---
+++ main_graph.py.bak
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+test for pyprofile
+"""
+from main_profile import *
+
+
+def main():
+    """
+    main
+    """
+    meth1()
+    meth2()
+    meth3()
+    meth4()
+    aggregate()
+
+
+if __name__ == "__main__":
+    main()
--- pyprofiler/__init__.py
+++ pyprofiler/__init__.py
@@ -4,5 +4,11 @@ pyprofiler
 Active8 (04-03-15)
 license: GNU-GPL2
 """
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from future import standard_library
+standard_library.install_aliases()
 
-from pyprofiler import *
+from .pyprofiler import *
