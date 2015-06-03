6b982fe64f47a0d98755597328c3ceaed85bd17c
Erik de Jonge <erik@a8.nl>
2015-06-03 13:41:55 +0200

    Ludwig Mies van der Rohe: God is in the details. Wednesday 03 June 2015 (week:22 day:154), 13:41:54

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='pyprofiler',
-      version='46',
+      version='47',
       description='Profiler utility for python, graphical and textual, whole program or segments',
       url='https://github.com/erikdejonge/pyprofiler',
       author='Erik de Jonge',
