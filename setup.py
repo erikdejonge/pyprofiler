3535adc63d25fab8416e78b65dd9190f94ec8dc5
Erik de Jonge <erik@a8.nl>
2015-06-16 15:50:03 +0200

    Tuesday 16 June 2015 (week:24 day:167), 15:50:02

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='pyprofiler',
-      version='50',
+      version='51',
       description='Profiler utility for python, graphical and textual, whole program or segments',
       url='https://github.com/erikdejonge/pyprofiler',
       author='Erik de Jonge',
       author_email='erik@a8.nl',
       license='GPL',
-      packages=['pyprofiler'],
+      packages=[b'pyprofiler'],
       zip_safe=True,
       classifiers=[
           "Programming Language :: Python",
