eca198f3be989fe235a3b13f4fde1b83ab71fa66
Erik de Jonge <erik@a8.nl>
2015-06-16 15:59:32 +0200

    Peter Drucker: Nothing that is Gods is obtainable by money. Tuesday 16 June 2015 (week:24 day:167), 15:59:30

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='pyprofiler',
-      version='51',
+      version='52',
       description='Profiler utility for python, graphical and textual, whole program or segments',
       url='https://github.com/erikdejonge/pyprofiler',
       author='Erik de Jonge',
       author_email='erik@a8.nl',
       license='GPL',
-      packages=[b'pyprofiler'],
+      packages=['pyprofiler'],
       zip_safe=True,
       classifiers=[
           "Programming Language :: Python",
